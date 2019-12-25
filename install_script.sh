cbda61b85b6d4f6f8423f335b9b14cbd5888ef20
Push Chen <littlepush+github@gmail.com>
2019-12-25 12:10:58 +0800

    update install peco script

--- install_script.sh
+++ install_script.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-curl -o- -LS "https://github.com/littlepush/PECoAutoStage/archive/master.zip" | tar -xz - -C .
+curl -o- -LS "https://github.com/littlepush/PECoAutoStage/archive/master.zip" >/dev/null | tar -xz - -C .
 cd PECoAutoStage-master
 chmod +x configure
 ./configure --enable-install-peco $1 && make && make install
