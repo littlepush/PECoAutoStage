6b91578e0051a7f1aed85708b23da2be968a1fac
Push Chen <littlepush+github@gmail.com>
2019-12-25 13:38:18 +0800

    update install script

--- install_script.sh
+++ install_script.sh
@@ -1,6 +1,39 @@
 #! /bin/bash
 
-curl -o- -LS "https://github.com/littlepush/PECoAutoStage/archive/master.zip" >/dev/null | tar -xz - -C .
-cd PECoAutoStage-master
-chmod +x configure
-./configure --enable-install-peco $1 && make && make install
+PREFIX=${PREFIX}
+[[ ! ${PREFIX} ]] && PREFIX=/usr
+
+touch ${PREFIX}/bin/__coas >/dev/null 2>&1
+if [ $? -eq 0 ]; then
+    MAKE_INSTALL="make install"
+    rm ${PREFIX}/bin/__coas
+else
+    MAKE_INSTALL="sudo make install"
+fi
+
+function __install_peco() {
+    curl -o ./$1.zip -L "https://github.com/littlepush/$1/archive/master.zip"
+    unzip -u ./$1.zip && rm ./$1.zip
+    cd ./$1-master
+    chmod +x configure
+    ./configure --prefix=${PREFIX} && make && ${MAKE_INSTALL}
+    RET=$?
+    cd -
+    rm -rf ./$1-master
+    [[ $? -ne 0 ]] && exit $RET
+}
+
+# Check if already has peutils
+[[ ! -f ${PREFIX}/include/peutils/peutils.h ]] && {
+    __install_peco PEUtils
+}
+
+[[ ! -f ${PREFIX}/include/pecotask/cotask.h ]] && {
+    __install_peco PECoTask
+}
+
+[[ ! -f ${PREFIX}/include/peconet/conet.h ]] && {
+    __install_peco PECoNet
+}
+
+__install_peco PECoAutoStage
